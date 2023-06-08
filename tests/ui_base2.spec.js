25b6c7d666040a76f496b0d617902011ad757958
Aliaksandr Navitsky <aliaksandr.navitsky@itechart-group.com>
2023-06-08 09:56:12 +0200

    dropdown

--- tests/ui_base2.spec.js
+++ tests/ui_base2.spec.js
@@ -1,16 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
-async function addItemByText(elements, textToClick) { // USING iterable $$  - const elements =  await page.$$('.card-body'); 
+async function addItemByTextIterable(elements, textToClick) { // USING iterable $$  - const elements =  await page.$$('.card-body'); 
     for (const element of elements) {                 // must be after await page.waitForLoadState('networkidle') - on  new page
         await (await element.$('b')).textContent() === textToClick &&
             await (await element.$('.btn.w-10.rounded')).click();
-    }
-}
-
-async function addItemByText2(elements, textToClick) { // USING non iterable locator()  - const elements = page.locator('.card-body') 
-    for (let i = 0; i < await elements.count(); i++) {
-        await elements.nth(i).locator('b').textContent() === textToClick &&
-            await elements.nth(i).locator('.btn.w-10.rounded').click()
     }
 }
 
@@ -21,6 +14,22 @@ async function elementsList(elements) { // USING non iterable locator()  - const
     }
     return hashes
 }
+
+async function addItemByTextNonIterable(elements, textToClick) { // USING non iterable locator()  - const elements = page.locator('.card-body') 
+    for (let i = 0; i < await elements.count(); i++) {
+        await elements.nth(i).locator('b').textContent() === textToClick &&
+            await elements.nth(i).locator('.btn.w-10.rounded').click()
+    }
+}
+
+async function selectDropOption(dropOptions, textToClick) { 
+    for (let i = 0; i < await dropOptions.count(); i++) {
+        (await dropOptions.nth(i).textContent()).trim() === textToClick &&
+            await dropOptions.nth(i).click()
+    }
+}
+
+
 
 // browser - fixture that confgured in config.js
 test('browser context playwright test', async ({ browser }) => {
@@ -41,9 +50,35 @@ test('browser context playwright test', async ({ browser }) => {
     console.log(allTitlesContent)
 
     //LOOP and click
-    const textToClick = 'zara coat 3';
-    await addItemByText2(elements, textToClick)
-    console.log('HASHES: ', await elementsList(elements))
+    const productName = 'zara coat 3';
+    const productName2 = 'adidas original';
+    await addItemByTextNonIterable(elements, productName)
+    await addItemByTextNonIterable(elements, productName2)
+
+    await page.locator('[routerlink*="cart"]').click()
+    await page.locator('div li').first().waitFor() // div li only if more than 1 product in the cart
+    await page.locator(`h3:has-text("${productName}")`).isVisible() // isVisible is not AUTOWAIT
+    await page.locator('text="Checkout"').click()
+
+    //DROPDOWN
+    await page.locator('[placeholder*="Country"]').type('ind', { delay: 100 }) // type slowly to get suggestions dropdown
+    const dropdown = page.locator('.ta-results')
+    await dropdown.waitFor()
+    const dropOptions = dropdown.locator('button')
+    await selectDropOption(dropOptions, 'India')
+
+
+
+
+
+
+
+
+
+
+
+
+
     await page.pause()
 })
 
